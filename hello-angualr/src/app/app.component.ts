c0fddc9b37c42a71c49bf6b0b5aa6f718f881ec8
bsnpbqd <wenbo-wb.bao@aia.com>
2018-01-25 15:46:42 +0800

    Add  `Show the hero`

--- hello-angualr/src/app/app.component.ts
+++ hello-angualr/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1> <h2>{{hero}} details</h2>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
